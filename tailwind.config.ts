07fd4e73701cd76c8fcc3915c0ec8b896dca7a62
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-02-27 20:25:32 +0530

    tailwind deployment settings changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
